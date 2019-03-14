890100659f545d1b43e5404224cf4c019a6f20c3
Manohar Castelino <manohar.r.castelino@intel.com>
2019-03-14 15:57:02 -0700

    Reconcile the differences between all workloads
    Do 10ms of CPU busybox in all services no matter how they
    are deployed
    
    Signed-off-by: Manohar Castelino <manohar.r.castelino@intel.com>

--- http/server/run_chain.sh
+++ http/server/run_chain.sh
@@ -1,9 +1,10 @@
 go build
-docker rm -f zipkin-latency prometheus-latency
+docker rm -f zipkin-latency prometheus-latency grafana-latency
 killall server
 
 docker run --name=zipkin-latency -d -p 9411:9411 --net=host openzipkin/zipkin
 docker run --name=prometheus-latency -d -p 9090:9090 --net=host -v $PWD/prometheus.yml:/etc/prometheus/prometheus.yml  prom/prometheus
-UPSTREAM_URI=localhost:8888 DOWNSTREAM_URI=http://localhost:8889 METRICS_PORT=8887 SERVICE_NAME=root ./server &
-UPSTREAM_URI=localhost:8889 DOWNSTREAM_URI=http://localhost:8890 METRICS_PORT=8886 SERVICE_NAME=branch ./server &
-UPSTREAM_URI=localhost:8890                                      METRICS_PORT=8885 SERVICE_NAME=leaf ./server &
+docker run -d --name=grafana-latency --net=host -p 3000:3000 grafana/grafana
+UPSTREAM_URI=localhost:8888 DOWNSTREAM_URI=http://localhost:8889 CPU_BUSYWORK=10 METRICS_PORT=8887 SERVICE_NAME=root ./server &
+UPSTREAM_URI=localhost:8889 DOWNSTREAM_URI=http://localhost:8890 CPU_BUSYWORK=10 METRICS_PORT=8886 SERVICE_NAME=branch ./server &
+UPSTREAM_URI=localhost:8890                                      CPU_BUSYWORK=10 METRICS_PORT=8885 SERVICE_NAME=leaf ./server &
